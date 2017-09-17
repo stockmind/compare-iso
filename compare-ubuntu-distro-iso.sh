ec71ffab2f02d2ab8a960ce14a83850ae3b9a471
stockmind <slysimo@gmail.com>
2017-09-17 04:32:19 +0200

    Handle auto extract of files

--- compare-ubuntu-distro-iso.sh
+++ compare-ubuntu-distro-iso.sh
@@ -6,31 +6,81 @@ if [ $EUID != 0 ]; then
     exit $?
 fi
 
+EXTRACT=true
+FIRST=false
+
+# Check arguments
+for i in "$@" ; do
+    if [[ $i == "extract" ]] ; then
+        echo "Extract different files from second iso"
+        EXTRACT=true
+        continue
+    fi
+    if [[ $i == "first" ]] ; then
+        echo "Extract different files from first iso"
+        FIRST=true
+        continue
+    fi
+done
+
 ISO1=$1
 ISO2=$2
 
+# Init environment
 mkdir -p iso1
 mkdir -p iso2
 mkdir -p filesystem1
 mkdir -p filesystem2
 
+# Mount base iso
 echo "Trying to mount $ISO1 ..."
 mount "$ISO1" ./iso1
 echo "Trying to mount $ISO2 ..."
 mount "$ISO2" ./iso2
 
+# Mount internal filesystem layer
 mount ./iso1/casper/filesystem.squashfs ./filesystem1
 mount ./iso2/casper/filesystem.squashfs ./filesystem2
 
+# Look for differences
 rsync --recursive --delete --links --checksum --verbose --dry-run ./iso1/ ./iso2/ > isodiff.txt
 rsync --recursive --delete --links --checksum --verbose --dry-run ./filesystem1/ ./filesystem2/ > filesystemdiff.txt
 
-umount ./iso1
-umount ./iso2
+# Remove unnecessary output or obvious differences like kernel files from output
+sed -i '/skipping non-regular file/d' filesystemdiff.txt
+sed -i '/sending incremental file list/d' filesystemdiff.txt
+sed -i '/lib\/modules\/[0-9]*.[0-9]*.[0-9]*-[^/]*\/*/d' filesystemdiff.txt
+sed -i '/sent [,.0-9]* bytes/d' filesystemdiff.txt
+sed -i '/total size/d' filesystemdiff.txt
+
+if [[ "$EXTRACT" = true ]]; then
+    FILE="filesystemdiff.txt"
+    DESTINATIONFOLDER="extracted"
+    SOURCEFOLDER="./filesystem2"
+    
+    # Clean previous run
+    rm -frd "$DESTINATIONFOLDER"
+    mkdir -p "$DESTINATIONFOLDER"
+    
+    if [[ "$FIRST" = true ]]; then
+        SOURCEFOLDER="./filesystem1"
+    fi
+    
+    # Read files one by one and copy them on destination folder
+    while IFS='' read -r line || [[ -n "$line" ]]; do
+        echo "Copy file: $line"
+        cp --parents "$SOURCEFOLDER""$line" "$DESTINATIONFOLDER"
+    done < "$FILE"
+
+fi
+
+# Unmount everything and delete temp directories
 umount ./filesystem1
 umount ./filesystem2
+umount ./iso1
+umount ./iso2
 
-rm -dfr iso1
-rm -dfr iso2
 rm -dfr filesystem1
 rm -dfr filesystem2
+rm -dfr iso1
+rm -dfr iso2
